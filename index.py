c0e268f1be5975f890e90ad82470513014df5b26
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2024-01-25 14:51:44 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,20 +10,18 @@ USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100
 MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 
 def query(content, page):
-    num = 1
     result = []
     content = content.replace(' ', '+')
-    for num in range(page):
-        URL = f"{google_mirror}/search?q={content}&start={num}"
-        headers = {"User-Agent": USER_AGENT}
-        resp = requests.get(URL, headers = headers, verify = False)
-        if resp.status_code == 200:
-            tree = etree.HTML(resp.text)
-            title = (tree.xpath('//*[@id="rso"]/div/div/div/div[1]/div/div/span/a/h3/text()') + tree.xpath('//*[@id="rso"]/div/div/div/div/div/div/div[1]/div[1]/div/div/span/a/h3/text()'))
-            href = (tree.xpath('//*[@id="rso"]/div/div/div/div[1]/div/div/span/a/@href') + tree.xpath('//*[@id="rso"]/div/div/div/div/div/div/div[1]/div[1]/div/div/span/a/@href'))
-            for i in range(len(title)):
-                result.append({'title': title[i], 'href': href[i]})
-    return result
+    URL = f"{google_mirror}/search?q={content}&start={page}"
+    headers = {"User-Agent": USER_AGENT}
+    resp = requests.get(URL, headers = headers, verify = False)
+    if resp.status_code == 200:
+        tree = etree.HTML(resp.text)
+        title = (tree.xpath('//*[@id="rso"]/div/div/div/div[1]/div/div/span/a/h3/text()') + tree.xpath('//*[@id="rso"]/div/div/div/div/div/div/div[1]/div[1]/div/div/span/a/h3/text()'))
+        href = (tree.xpath('//*[@id="rso"]/div/div/div/div[1]/div/div/span/a/@href') + tree.xpath('//*[@id="rso"]/div/div/div/div/div/div/div[1]/div[1]/div/div/span/a/@href'))
+        for i in range(len(title)):
+            result.append({'title': title[i], 'href': href[i]})
+    return list(set(result))
 
 app = Flask(__name__)
 
@@ -34,6 +32,9 @@ def index():
 @app.route('/search')
 def search():
     content = request.values.get('query')
+    page = request.values.get('page')
     if content is None:
         return redirect('/')
-    return render_template('search.html', results = query(content, read_pages))
+    if page is None:
+        page = 1
+    return render_template('search.html', results = query(content, page))
